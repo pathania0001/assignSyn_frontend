708bcb20582564f31f0ce485d8418d17e53260a6
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2026-01-14 22:38:12 +0530

    update url

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://assignsyn-backend.onrender.com/api"
+  baseURL: "http://assignsyn-backend.onrender.com/api/users"
 });
 
 export const fetchUsers = () => api.get("/");
