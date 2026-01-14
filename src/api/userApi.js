b2f1f806a82b04ee968b2b5a689fc2228396f1e8
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2026-01-14 22:24:52 +0530

    url change

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/users"
+  baseURL: "http://assignsyn-backend.onrender.com/"
 });
 
 export const fetchUsers = () => api.get("/");
